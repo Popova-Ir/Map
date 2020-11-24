c9e351528b6439ab9fe33eb4a3a40624226356b9
Popova-Ir <73823517+Popova-Ir@users.noreply.github.com>
2020-11-24 13:54:05 +0500

    Add files via upload

---
+++ Maps/maps/maps/LocationVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  maps
+//
+//  Created by user on 24/11/2020.
+//
+
+import UIKit
+
+class LocationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
